bf0636967d0b75978bee36d234b4ec353f58c64a
ayerest <irisiseve@gmail.com>
2019-09-16 16:46:45 -0700

    finished react event system lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+    sayGood = () => {
+        console.log("Good!")
+    }
+
+    sayHey = () => {
+        console.log("Hey! Eyes on me!")
+    }
+    render() {
+        return (
+            <button
+                onFocus={this.sayGood}
+                onBlur={this.sayHey}> 
+            </button>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+
+    passWFn = () => {
+        console.log("Entering password...")
+    }
+    render() {
+        return(
+            <input 
+            type='password'
+            onKeyUp={this.passWFn}/>
+        )
+    }
+}
+
+export default Keypad;
